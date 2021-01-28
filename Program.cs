45508f33940d4bdb72c6806a40d487f3675e1ac2
jonatas-lopes <jonatas.lopes95@live.com>
2021-01-28 13:48:15 -0300

    inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consumoEnergia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
